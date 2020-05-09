c71305f7165aa76342ec43824c07a11e4b7adb99
kmkm37 <60097820+kmkm37@users.noreply.github.com>
2020-05-10 02:05:13 +0900

    2020.05.10 locate blade and css file kyungjin(2)

---
+++ resources/views/mypage.blade.php
@@ -0,0 +1,42 @@
+@include('topflower')
+<div class="menu4">
+<h3 align="center">마이페이지</h3>
+<hr align="left" class="one"></hr>
+</div>
+<div class="menu5">
+  <div class="myinfo">
+  <h4>내 정보</h4>
+</myinfo>
+  <div class="privacy">
+    <div class="table">
+<table cellpadding="10" cellspacing="10" width="100%">
+  <tr>
+    <th>ID</th>
+    <td>정경진</td>
+  </tr>
+  <tr>
+    <th>PW</th>
+    <td>******</td>
+  </tr>
+  <tr>
+    <th>이름</th>
+    <td>정경진</td>
+  </tr>
+  <tr>
+    <th>연락처</th>
+    <td>*****</td>
+  </tr>
+  <tr>
+    <th>이메일</th>
+    <td>asdsad@naver.com</td>
+  </tr>
+  <tr>
+    <th>주소</th>
+    <td>ㅁㄴㄹㄴㅁㄹㄴㅁㄹ</td>
+  </tr>
+  </div>
+</div>
+</div>
+</div>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/locate1', function () {
+    return view('locate');
+});
+Route::get('/mypage1', function () {
+    return view('mypage');
+});
