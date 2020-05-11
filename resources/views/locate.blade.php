ccc94e4f5d01be97c9dc43d2895b68acd7f9d1f5
kyungjinJEONG <60097820+kmkm37@users.noreply.github.com>
2020-05-11 17:29:23 +0900

    jungkyungjin

--- resources/views/locate.blade.php
+++ resources/views/locate.blade.php
@@ -3,28 +3,15 @@
             <h3>내 주변 꽃집</h3>
             <hr align="left" class="one">
             </hr>
-            <div class="dropdown2">
-              <!-- onclick, call the Vanilla function for this one -->
-              <button onclick="myFunction(this);" class="dropbtn2">시/도</button>
-              <div id="myDropdown1" class="dropdown2-content">
-                <a href="#home">서울시</a>
-                <a href="#about">부산시</a>
-                <a href="#contact">경기도</a>
-              </div>
-            </div>
-            <!-- Added 2nd dropdown ! -->
-            <div class="dropdown2">
-              <!-- onclick, call the jQuery function for this one -->
-              <button onclick="myJQueryFunction(this);" class="dropbtn2">군/구</button>
-              <div id="myDropdown2" class="dropdown2-content">
-                <a href="#home">서대문구</a>
-                <a href="#about">종로구</a>
-                <a href="#contact">은평구</a>
-              </div>
-            </div>
-            <hr align="left" class="one">
-            </hr>
-        </div>
+            <select id="input" onchange="random_function()">
+       <option>지역선택</option>
+       <option>서울</option>
+       <option>부산</option>
+   </select>
+   <div>
+      <select id="output" onchange="random_function1()">
+   </div>
+
         <div class="menu5">
             <div class="shopname">
                 <h3>ccit3</h3>
@@ -49,37 +36,28 @@
             </div>
         </div>
     </div>
-@include('footer')
+</body>
+</html>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-<script type="text/javascript">
-function myJQueryFunction(element) {
-  var elements = ".dropdown2-content";
-  $(elements).removeClass('show');
-  $(element).next(elements).toggleClass("show");
-}
+<script>
+    function random_function()
+    {
+        var a=document.getElementById("input").value;
+        if(a==="서울")
+        {
+            var arr=["서대문구","종로구"];
+        }
+        else if(a==="부산")
+        {
+            var arr=["해운대구","서구","동구"];
+        }
 
-/* Javascript only */
-function myFunction(element) {
-  var dropdowns = document.getElementsByClassName("dropdown2-content");
-  var i;
-  for (i = 0; i < dropdowns.length; i++) {
-    dropdowns[i].classList.remove('show');
-  }
-  // element.nextSibling is the carriage return… need to go to the next next to point on the dropdown menu
-  element.nextSibling.nextSibling.classList.toggle("show");
-}
+        var string="";
 
-/* W3Schools function to close the dropdown when clicked outside. */
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn2')) {
-    var dropdowns = document.getElementsByClassName("dropdown2-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
+        for(i=0;i<arr.length;i++)
+        {
+            string=string+"<option value="+arr[i]+">"+arr[i]+"</option>";
+        }
+        document.getElementById("output").innerHTML=string;
     }
-  }
-}
 </script>
