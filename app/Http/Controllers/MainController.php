bb569188afb2d6d712593a5cd77448af91ee07f7
GMSWnnk <58356312+GMSWnnk@users.noreply.github.com>
2020-05-13 17:25:25 +0900

    폴더화 및 정리 -- jisuEO

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -8,9 +8,18 @@ use Illuminate\Http\Request;
 class MainController extends Controller
 {
   public function main(){
-      return view('index');
+    return view('mainlayout');
   }
-  public function login(){
-    return view('login');
+  public function login_customer(){
+    return view('login.login_customer');
+  }
+  public function login_seller(){
+    return view('login.login_seller');
+  }
+  public function register_costomer(){
+    return view('register.register_costomer');
+  }
+  public function register_seller(){
+    return view('register.register_seller');
   }
 }
