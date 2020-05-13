b2d02df538232ebc693ffe4a37ee9270c3ac6e20
GMSWnnk <58356312+GMSWnnk@users.noreply.github.com>
2020-05-13 17:24:37 +0900

    폴더로 합치고 네임정리 -- jisuEO

---
+++ resources/views/find_information/find_pw_way.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html> <!--박소현 계속 수정중 -->
+<html lang="en" dir="ltr">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" type="text/css" href="/css/login.css">
+  <title>비밀번호 찾기</title>
+
+</head>
+<body>
+
+  <div id="all">
+    <div class="text">
+      <h1> 비밀번호 찾기 </h1>
+      <hr class = way>
+      비밀번호를 찾을 방법을 선택해 주세요.
+    </div>
+
+    <div class ="find_pw_way">
+      <p>
+        <form action = '처리할 주소' method='POST'>
+          이름 : <input type="name" name="name"required ><br>
+          전화번호 : <input type="tell" name="tell" size=20 required title="000-0000-0000"><input type="submit" value="인증번호받기"><br>
+          <input type="name" autofocus placeholder="인증번호 입력" name="name" required >
+          <input type="submit" value="확인"><br>
+          <p>
+            <button type="submit" > <a href="http://laravel.site/pw_reset">다음</a> </button>
+          </p>
+        </form>
+      </p>
+    </div>
+
+  </div>
+
+  @include('footer')
+</body>
+</html>
