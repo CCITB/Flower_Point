13b2a94e5f5ba337f3526e6cdc5d883c02512078
gwang jin <kflrm1234@naver.com>
2020-05-14 20:24:43 +0900

    gwang jin code upload

---
+++ resources/views/gwang jin/postscript make out2 gj.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>상품 후기 ㅣ 꽃갈피</title>
+    <style> .centered { display: table; margin-left: auto; margin-right: auto; } </style>
+    <div class="centered">
+  </head>
+  <body>
+  @include('header')
+  <h1>후기 작성</h1>
+  <u>　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　</u>
+  <button type="submit" class="btn btn-default">가게명 : ccit 1　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　</button></li>
+  <br>
+  <button type="submit" class="btn btn-default">제목 : 　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　</button></li> 　　　　　　　　　
+  <br>
+  <button type="submit" class="btn btn-default">내용 　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　</button></li>
+  <br>
+  <button type="submit" class="btn btn-default">목록</button>　　　　　　　　 <button type="submit" class="btn btn-default">꽃.jpg　　　　　　　　　　　　　　</button>　　　　<button type="submit" class="btn btn-default">첨부파일</button>　　<button type="submit" class="btn btn-default">저장</button>　　<button type="submit" class="btn btn-default">취소</button>
+</body>
+</html>
