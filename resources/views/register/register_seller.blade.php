e494316f3e72c49d7f6ce3e366fc834a5c393bcc
GMSWnnk <58356312+GMSWnnk@users.noreply.github.com>
2020-05-14 17:14:51 +0900

    RegisterSeller 수정--jisuEO

--- resources/views/register/register_seller.blade.php
+++ resources/views/register/register_seller.blade.php
@@ -15,7 +15,7 @@
       <hr class = way>
     </div>
     <div class="signup">
-      <form action = '/RegisterControllerSeller' method="post" name="register_form" onsubmit="check_signup">
+      <form action = '/RegisterControllerSeller' method="post" name="registerform" onsubmit='return check_signup();'>
         @csrf
         <table>
           <tr>
@@ -31,94 +31,95 @@
             <td><input type="password" placeholder="Password" name="s_password" id="new_pw" required ></td>
           </tr>
           <tr>
-          <th>비밀번호 확인</th>
-        </tr>
-        <tr>
-        <td><input type="password" placeholder="Password" name="s_re_password" id="check" required ></td>
-      </tr>
-      <tr>
-        <th>이름</th>
-      </tr>
-      <tr>
-        <td><input type="name" placeholder="Name" id="name" name="s_name" required></td>
-      </tr>
-      <tr>
-        <th>연락처</th>
-      </tr>
-      <tr>
-        <td><input type="text" placeholder="Phone Number" id="s_phonenum" name="s_phonenum" required></td>
-        <td><button type="button" value="인증번호" id="certification">인증번호</button></td>
-      </tr>
-      <tr>
-        <th>
-          성별
-        </th>
-      </tr>
-      <td>
-        <select class="form_select" name="s_gender" required>
-          <option value="">성별</option>
-          <option value="남성">남성</option>
-          <option value="여성">여성</option>
-        </select>
-      </td>
-      <tr>
-        <th>
-          생년월일
-        </th>
-      </tr>
-      <tr>
-        <td><input type=text placeholder="ex)200514" id="birth" name="s_birth"></td>
-      </tr>
-      <tr>
-        <th>주소</th>
-      </tr>
-      <tr>
-        <td><input type="text" placeholder="Address" id="address" name="s_address" required></td>
-      </tr>
-      <tr>
-        <th>이메일</th>
-      </tr>
-      <tr>
-        <td><input type="email" placeholder="email "id="email" name="s_email"  required></td>
-      </tr>
-      <tr>
-        <!-- <button type="button" style="border-radius:5px; font-s"/> <a href="http://laravel.site/login">돌아가기</a> </button> </td> -->
-        <td><button type="button" onclick="history.back()">돌아가기</button></td>
-        <td><input type="submit" value="다음"></td>
-      </tr>
-    </table>
-  </form>
-</div>
-<script type="text/javascript">
-function check_signup(){
-  var registerform = document.forms['registerform'];
+            <th>비밀번호 확인</th>
+          </tr>
+          <tr>
+            <td><input type="password" placeholder="Password" name="s_re_password" id="check" required ></td>
+          </tr>
+          <tr>
+            <th>이름</th>
+          </tr>
+          <tr>
+            <td><input type="name" placeholder="Name" id="name" name="s_name" required></td>
+          </tr>
+          <tr>
+            <th>연락처</th>
+          </tr>
+          <tr>
+            <td><input type="text" placeholder="Phone Number" id="s_phonenum" name="s_phonenum" required></td>
+            <td><button type="button" value="인증번호" id="certification">인증번호</button></td>
+          </tr>
+          <tr>
+            <th>
+              성별
+            </th>
+          </tr>
+          <td>
+            <select class="form_select" name="s_gender" required>
+              <option value="">성별</option>
+              <option value="남성">남성</option>
+              <option value="여성">여성</option>
+            </select>
+          </td>
+          <tr>
+            <th>
+              생년월일
+            </th>
+          </tr>
+          <tr>
+            <td><input type="text" placeholder="ex)200514" id="birth" name="s_birth"></td>
+          </tr>
+          <tr>
+            <th>주소</th>
+          </tr>
+          <tr>
+            <td><input type="text" placeholder="Address" id="address" name="s_address" required></td>
+          </tr>
+          <tr>
+            <th>이메일</th>
+          </tr>
+          <tr>
+            <td><input type="email" placeholder="email "id="email" name="s_email"  required></td>
+          </tr>
+          <tr>
+            <!-- <button type="button" style="border-radius:5px; font-s"/> <a href="http://laravel.site/login">돌아가기</a> </button> </td> -->
+            <td><button type='button' onclick="history.back()">돌아가기</button></td>
+            <td><input type='submit' value="다음"></td>
+          </tr>
+        </table>
+      </form>
+    </div>
 
-  if(registerform['s_id'].value.length<5){
-    return
-    alert('아이디를 5자 이상 입력하세요.');
-  }
-  if(registerform['s_password'].value.length<5){
-    alert('비밀번호를 5자 이상 입력하세요.');
-    return false;
-  }
-  if(registerform['s_password'].value == registerform['s_re_password'].value){
-    alert('비밀번호가 동일하지 않습니다.');
-    return false;
-  }
-  if(registerform['s_name'].value.length<1){
-    alert('이름을 입력하세요.');
-    return false;
-  }
-  if(registerform['s_phonenum'].value.length<1){
-    alert('연락처를 입력하세요.');
-    return false;
-  }
-  if(registerform['s_email'].value.length<1){
-    alert('이메일을 입력하세요.');
-    return false;
-  }
+    <script type="text/javascript">
+    function check_signup(){
+      var registerform = document.forms['registerform'];
 
-  else {
-    return true;
-  }
-}
+      if(registerform['s_id'].value.length<5){
+        alert('아이디를 5자 이상 입력하세요.');
+        return false;
+      }
+      if(registerform['s_password'].value.length<5){
+        alert('비밀번호를 5자 이상 입력하세요.');
+        return false;
+      }
+      if(registerform['s_password'].value = registerform['s_re_password'].value){
+        alert('비밀번호가 동일하지 않습니다.');
+        return false;
+      }
+      if(registerform['s_name'].value.length<1){
+        alert('이름을 입력하세요.');
+        return false;
+      }
+      if(registerform['s_phonenum'].value.length<1){
+        alert('연락처를 입력하세요.');
+        return false;
+      }
+      if(registerform['s_email'].value.length<1){
+        alert('이메일을 입력하세요.');
+        return false;
+      }
+      else {
+        return true;
+      }
+    }
+    </script>
