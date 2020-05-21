93371419c2bd923615ddadf33f062875c5826dc1
sohyun <64099537+So-Hyun-Park@users.noreply.github.com>
2020-05-21 11:39:28 +0900

    buy-information -- sohyun

--- resources/views/Buy_information.blade.php
+++ resources/views/Buy_information.blade.php
@@ -29,53 +29,56 @@
         </div>
         <hr>
 
-        <div class="shop_basic">
-          <div class = "shop_name">
-            <a href="/해당매장">ccit 1</a> <!-- 해당 매장 테이블에서 불러와 링크걸기 -->
-          </div>
-          <div claas = "shop_location">
+        <div class="shop_basic"> <!-- 해당 매장 테이블에서 불러와 링크걸기 -->
+          <div class="shop_name"><a href="/해당매장">ccit 1</a></div>
+          <div class="shop_lo">
             서울시 종로구
           </div>
+          <div class="pd_price"> <!-- 상품 가격 불러오기 -->
+            <strong>10,000원</strong>
+          </div>
         </div>
 
-        <div class="pd_price">
-          <br><strong>10000원</strong> <!-- 상품 가격 불러오기 -->
-        </div>
-
-        <p>
+        <div class="pd_deliver">
           <div class="pd_deliver1">
-            배송비<br>
+            <span class="del_text">배송비</span><br>
             2500원
           </div>
           <div class="pd_deliver2">
-            배송기간<br>
+            <span class="del_text">배송기간</span><br>
             2일 이내
           </div>
           <div class="pd_deliver3">
-            적립금<br>
+            <span class="del_text">적립금</span><br>
             최대 2%
           </div>
-        </p>
-
-        <div class="total_price_text">
-          총 금액
-        </div>
-        <div class="total_price">
-          ~원
-        </div>
-        <div class="order_bt">
-          <button class="convenience" type="button" onclick="location.href = '/결제창'">주문</button>
         </div>
 
-        <select class="select_option" name="select_option">
-          <option value="option 1">옵션 1</option>
-          <option value="option 2">옵션 2</option>
-        </select><br><br>
 
-        <select class="select_option" name="select_pack">
-          <option value="pack 1">포장 1</option>
-          <option value="pack 2">포장 2</option>
-        </select>
+        <div class="options">
+          <select class="select_option" name="select_option">
+            <option value="option 1">옵션 1</option>
+            <option value="option 2">옵션 2</option>
+          </select>
+
+          <select class="select_option" name="select_pack">
+            <option value="pack 1">포장 1</option>
+            <option value="pack 2">포장 2</option>
+          </select>
+        </div>
+
+
+        <div class="pd_pay">
+          <div class="total_price_text">
+            총 금액
+          </div>
+          <div class="total_price">
+            ~원
+          </div>
+          <div class="order_bt">
+            <button class="order" type="button" onclick="location.href = '/결제창'">주문</button>
+          </div>
+        </div>
 
       </form>
     </div>
