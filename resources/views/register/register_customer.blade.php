1ce341bd4dce0e826459bd4f4a39209adee99967
jisuEO <58356312+GMSWnnk@users.noreply.github.com>
2020-06-02 01:05:36 +0900

    유저 회원가입(수정중) --jisuEO

--- resources/views/register/register_customer.blade.php
+++ resources/views/register/register_customer.blade.php
@@ -34,29 +34,46 @@
         <div class="check_div" id="re_pw_check" value=""></div>
 
         <div class="sign_name">이름</div>
-        <input class="inf1" type="name" placeholder="Name" id="name" name="c_name" >
+        <input class="inf1" type="name" placeholder="Name" id="c_name" name="c_name" >
         <div class="check_div" id="name_check" value=""></div>
 
         <div class="sign_name">연락처</div>
-        <input class="inf1" type="text" placeholder="Phone Number" id="phonenum" name="c_phonenum" >
+        <input class="inf1" type="text" placeholder="Phone Number" id="c_phonenum" name="c_phonenum" >
         <div class="check_div" id="phonenum_check" value=""></div>
 
         <div class="sign_name">생년월일</div>
-        <input class="inf1" type="text" placeholder="ex)200514" id="birth" name="c_birth">
-        <div class="check_div" id="phonenum_check" value=""></div>
+        <input class="inf2" type="text" placeholder="년(4자)" id="c_birth_y" name="c_birth_y" maxlength="4">
+
+        <select class="inf2" id="c_birth_m" name="c_birth_m">
+          <option value="">월</option>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">6</option>
+          <option value="7">7</option>
+          <option value="8">8</option>
+          <option value="9">9</option>
+          <option value="10">10</option>
+          <option value="11">11</option>
+          <option value="12">12</option>
+        </select>
+        <input class="inf2" type="int" placeholder="일" id="c_birth_d" name="c_birth_d" maxlength="2">
+        <div class="check_div" id="birth_check" value=""></div>
 
         <div class="gender">
           <div class="sign_name">성별</div>
-          <select class="form_select" name="c_gender" id=gender >
+          <select class="form_select" name="c_gender" id=c_gender >
             <option value="">성별</option>
             <option value="남성">남성</option>
             <option value="여성">여성</option>
           </select>
         </div>
-        <br>
+        <div class="check_div" id="gender_check" value=""></div>
 
         <div class="sign_name">이메일</div>
-        <input class="inf1" type="email" placeholder="email "id="email" name="c_email"  >
+        <input class="inf1" type="email" placeholder="email "id="c_email" name="c_email"  >
+        <div class="check_div" id="email_check" value=""></div>
       </div>
       <!-- <button type="button" style="border-radius:5px; font-s"/> <a href="http://laravel.site/login">돌아가기</a> </button> </td> -->
       <div class="under">
@@ -75,15 +92,34 @@
     }
   });
 
-  $("#id").blur(function() {
+  $("#id").keyup(function() {
     checkIdInput();
   });//blur
   $("#pw").blur(function() {
     checkPwInput();
   });//blur
-  $("#check").blur(function() {
+  $("#check").keyup(function() {
     checkRePwInput();
   });//blur
+  $("#name").blur(function() {
+    checkNameInput();
+  });//blur
+  $("#s_birth_y").blur(function() {
+    checkBirthInput()});//blur
+
+    $("#s_birth_m").change(function() {
+      checkBirthInput()
+    });//blur
+
+    $("#s_birth_d").blur(function() {
+      checkBirthInput();
+    });
+    $("#s_gender").change(function() {
+      checkGender();
+    });
+    $("#s_email").change(function() {
+      checkEmail();
+    });
 
   function checkIdInput(){
     var customer_id = $('#id').val();
@@ -136,6 +172,7 @@
       ,error : function() {   console.log("실패");  }
     }) //ajax
   }
+
   function checkPwInput(){
 
     var customer_pw = $('#pw').val();
@@ -144,7 +181,7 @@
     $.ajax({
 
       type: 'post',
-      url: 'customer_OverlapPW',
+      url: 'customer_Overlap',
       dataType: 'json',
       data: { "pw":customer_pw },
 
@@ -173,13 +210,14 @@
       ,error : function() {  console.log("pw실패");  }
     }) //ajax
   }
+
   function checkRePwInput(){
     var customer_re_pw = $('#check').val();
     var customer_pw = $('#pw').val();
     $.ajax({
 
       type: 'post',
-      url: 'customer_OverlapPW',
+      url: 'customer_Overlap',
       dataType: 'json',
       data: { "pw":customer_pw },
 
@@ -211,5 +249,145 @@
       }//success
       ,error : function() {  console.log("pw실패");  }
     }) //ajax
+  }
+
+  function checkNameInput(){
+    var customer_name = $('#name').val();
+    var markJ = /[\{\}\[\]\/?.,;:|\)*~`!^\-+<>@\#$%&\\\=\(\'\"\s]/gi;
+    var empty = /\s/gi;
+
+    $.ajax({
+      type: 'post',
+      url: 'customer_Overlap',
+      dataType: 'json',
+      data: { "name" : customer_name },
+
+      success : function(data) {
+        console.log(data);
+        //공백(빈칸)
+        if(customer_name == ""){
+          $('#name_check').text("필수 정보입니다.");
+          $('#name_check').css('color', 'red');
+          $('#btnSubmit').attr('disabled', true);
+        }
+        else if(!markJ.test(customer_name)){
+          $("#name_check").text("");
+          $('#btnSubmit').attr('disabled', false);
+        }
+        //특수기호, 공백(space) 사용불가
+        else{
+          $('#name_check').text("한글과 영문 대 소문자를 사용하세요.(특수기호, 공백 사용불가)");
+          $('#name_check').css('color', 'red');
+          $('#btnSubmit').attr('disabled', true);
+        }
+      }
+      ,error : function() {console.log("name실패");}
+    })
+  }
+
+  function checkBirthInput(){
+
+    var s_birth_y = $('#c_birth_y').val();
+    var s_birth_m = $('#c_birth_m').val();
+    var s_birth_d = $('#c_birth_d').val();
+    var birthJ =  /^[0-9]+$/
+    $.ajax({
+
+      type: 'post',
+      url: 'customer_Overlap',
+      dataType: 'json',
+      data: { "c_birth_y":c_birth_y },
+
+      success : function(data) {
+        console.log(c_birth_m);
+        //년도
+        if(birthJ.test(c_birth_y)&&c_birth_y.length>3){
+          $("#birth_check").text("");
+          $('#birth_check').css('color', 'red');
+          $('#btnSubmit').attr('disabled', false);
+          //월
+          if(c_birth_m==""){
+            $("#birth_check").text('태어난 월을 선택하세요.');
+            $('#birth_check').css('color', 'red');
+            $('#btnSubmit').attr('disabled', true);
+          }
+          else{
+            $('#birth_check').text('');
+            $('#birth_check').css('color', 'red');
+            $('#btnSubmit').attr('disabled', false);
+            //일
+            if(c_birth_d==""){
+              $("#birth_check").text('태어난 일(날짜) 2자리를 정확하게 입력하세요.');
+              $('#birth_check').css('color', 'red');
+              $('#btnSubmit').attr('disabled', true);
+            }
+            else{
+              $('#birth_check').text('');
+              $('#birth_check').css('color', 'red');
+              $('#btnSubmit').attr('disabled', false);
+            }
+          }
+        }
+        else{
+          $('#birth_check').text(' 태어난 년도 4자리를 정확하게 입력하세요. ');
+          $('#birth_check').css('color', 'red');
+          $('#btnSubmit').attr('disabled', true);
+        }
+
+      }//success
+      ,error : function() {  console.log("실패");  }
+    }) //ajax
+  }
+
+  function checkGender(){
+    var c_gender = $('#c_gender').val();
+
+    $.ajax({
+      type: 'post',
+      url: 'customer_Overlap',
+      dataType: 'json',
+      data: { "c_gender" : c_gender },
+
+      success : function(data) {
+        console.log(data);
+        //공백(빈칸)
+        if(!c_gender == ""){
+          $('#gender_check').text("");
+          $('#gender_check').css('color', 'red');
+          $('#btnSubmit').attr('disabled', false);
+        }
+        else{
+          $('#gender_check').text("필수 정보입니다.");
+          $('#btnSubmit').attr('disabled', true);
+        }
+      }
+      ,error : function() {console.log("실패");}
+    })
+  }
+
+  function checkEmail(){
+    var c_email = $('#c_email').val();
+
+    $.ajax({
+      type: 'post',
+      url: 'customer_Overlap',
+      dataType: 'json',
+      data: { "c_email" : c_email },
+
+      success : function(data) {
+        console.log(data);
+        //공백(빈칸)
+        if(!s_email == ""){
+          $('#email_check').text("");
+          $('#email_check').css('color', 'red');
+          $('#btnSubmit').attr('disabled', false);
+        }
+        else{
+          $('#email_check').text("필수 정보입니다.");
+          $('#btnSubmit').attr('disabled', true);
+        }
+      }
+      ,error : function() {console.log("실패");}
+    })
   }
   </script>
