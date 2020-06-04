01c1d5a12561539c3fdd7ca1d76874ee3e18185e
jisuEO <58356312+GMSWnnk@users.noreply.github.com>
2020-06-05 03:32:08 +0900

    판매자 회원가입 jquery부분 문제점 전체 수정(진행중) -- jisuEO

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -73,30 +73,30 @@ class RegisterController extends Controller
       $input = $request->input('id');
       $sellers = DB::table('seller')-> where('s_id','=',$input)->get()->count();
       return response()->json($sellers);
-
-      $overlap_pw = $request->input('pw');
-      return response()->json($overlap_pw);
-
-      $overlap_ck = $request->input('check');
-      return response()->json($overlap_ck);
-
-      $overlap_name = $request->input('name');
-      return response()->json($overlap_name);
-
-      $overlap_birth_y = $request->input('s_birth_y');
-      return response()->json($overlap_birth_y);
-
-      $overlap_birth_m = $request->input('s_birth_m');
-      return response()->json($overlap_birth_m);
-
-      $overlap_birth_d = $request->input('s_birth_d');
-      return response()->json($overlap_birth_d);
-
-      $overlap_s_gender = $request->input('s_gender');
-      return response()->json($overlap_s_gender);
-
-      $overlap_s_email = $request->input('s_email');
-      return response()->json($overlap_s_email);
+      //
+      // $overlap_pw = $request->input('pw');
+      // return response()->json($overlap_pw);
+      //
+      // $overlap_ck = $request->input('check');
+      // return response()->json($overlap_ck);
+      //
+      // $overlap_name = $request->input('name');
+      // return response()->json($overlap_name);
+      //
+      // $overlap_birth_y = $request->input('s_birth_y');
+      // return response()->json($overlap_birth_y);
+      //
+      // $overlap_birth_m = $request->input('s_birth_m');
+      // return response()->json($overlap_birth_m);
+      //
+      // $overlap_birth_d = $request->input('s_birth_d');
+      // return response()->json($overlap_birth_d);
+      //
+      // $overlap_s_gender = $request->input('s_gender');
+      // return response()->json($overlap_s_gender);
+      //
+      // $overlap_s_email = $request->input('s_email');
+      // return response()->json($overlap_s_email);
     }
 
     //[resister_seller jQuery부분] --어지수
