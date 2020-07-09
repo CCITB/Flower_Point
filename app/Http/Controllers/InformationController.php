4f58ad5d48854d59bd434034176cfe4768b19ca8
kmkm37 <60097820+kmkm37@users.noreply.github.com>
2020-07-09 21:18:08 +0900

    .

--- app/Http/Controllers/InformationController.php
+++ app/Http/Controllers/InformationController.php
@@ -219,13 +219,11 @@ class InformationController extends Controller
             ->join('store', 'seller.s_no', '=', 'store.seller_no')->select('*')
             ->where('s_no','=', $sellerprimary )->get();
             $proro = DB::table('product')
-            ->join('payment','payment.product_no','product.p_no')
             ->join('store','product.store_no','=','store.st_no')
             ->select('*')->where('st_no' ,'=', $data[0]->st_no)->where('p_status','등록')->get();
             $introduce = DB::table('store')->select('st_introduce')->where('st_no' ,'=' , $data[0]->st_no )->get();
             $store_address = DB::table('store_address')->select('*')->where('st_no' ,'=', $data[0]->st_no)->get();
             $detail_address = DB::table('store_address')->select('a_detail')->where('st_no' ,'=', $data[0]->st_no)->get();
-
             return view('myshop/shop_seller' , compact('data', 'proro','introduce', 'store_address', 'detail_address'));
       }
       else{
