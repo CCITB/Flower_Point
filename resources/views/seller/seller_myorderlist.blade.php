2626e59e4acd75d83c8daf3b15d7ce5413abce6a
jisuEO <58356312+GMSWnnk@users.noreply.github.com>
2020-07-13 02:23:31 +0900

    배송조회를 하고있었는데.. id값만 가져오면 끝남.. - jisu

--- resources/views/seller/seller_myorderlist.blade.php
+++ resources/views/seller/seller_myorderlist.blade.php
@@ -9,6 +9,7 @@
   <link rel="stylesheet" type="text/css" href="/css/orderlist.css">
   <link rel="stylesheet" href="//cdn.datatables.net/1.10.21/css/jquery.dataTables.min.css" type="text/css"/>
   <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+  <!-- <script src="https://info.sweettracker.co.kr/api/v1/companylist?t_key=2A8EDai0BMFebqhDkoYuPw" -H  "accept: application/json;charset=UTF-8"></script> -->
 </head>
 <body>
   @include('lib.header')
@@ -22,125 +23,155 @@
     <div class="myorderlist">
       <div class="myorderlist-top">
         <div class="myorderlist-infor">
-          <!--<table> 보류중
-          <tr>
-          <td rowspan="3" class="orderpicture">사진</td>
-          <td class="orderblink">입금대기</td>
-          <td class="ordercount">0</td>
-          <td class="orderspace">건</td>
-          <td rowspan="3" class="orderpicture">사진</td>
-          <td class="orderblink">배송준비</td>
-          <td class="ordercount">0</td>
-          <td class="orderspace">건</td>
-          <td rowspan="3" class="orderpicture">사진</td>
-          <td class="orderblink">취소요청</td>
-          <td class="ordercount">0</td>
-          <td class="orderspace">건</td>
-        </tr>
-        <tr>
-        <td>신규주문</td>
-        <td>0</td>
-        <td>건</td>
-        <td>배송중</td>
-        <td>0</td>
-        <td>건</td>
-        <td>반품요청</td>
-        <td>0</td>
-        <td>건</td>
-      </tr>
-      <tr>
-      <td>오늘출발</td>
-      <td>0</td>
-      <td>건</td>
-      <td>배송완료</td>
-      <td>0</td>
-      <td>건</td>
-      <td>교환요청</td>
-      <td>0</td>
-      <td>건</td>
-    </tr>
-  </table> -->
-  @if(count($order))
-  <div class="sellerorderlist">
-    <!-- <form class="" action="index.html" method="post" name="mycheck"> -->
-    <div class="orderlist-bottom">
-      <button type="submit" name="button" id="check" class="ordercheck" form="order_list">발주확인</button>
-      <button type="submit" name="button" id="send" class="sendmessage" form="order_list">발송처리</button>
-    </div>
+          <table>
+            <tr>
+              <td rowspan="3" class="orderpicture">사진</td>
+              <td class="orderblink">입금대기</td>
+              <td class="ordercount">0</td>
+              <td class="orderspace">건</td>
+              <td rowspan="3" class="orderpicture">사진</td>
+              <td class="orderblink">배송준비</td>
+              <td class="ordercount">0</td>
+              <td class="orderspace">건</td>
+              <td rowspan="3" class="orderpicture">사진</td>
+              <td class="orderblink">취소요청</td>
+              <td class="ordercount">0</td>
+              <td class="orderspace">건</td>
+            </tr>
+            <tr>
+              <td>신규주문</td>
+              <td>0</td>
+              <td>건</td>
+              <td>배송중</td>
+              <td>0</td>
+              <td>건</td>
+              <td>반품요청</td>
+              <td>0</td>
+              <td>건</td>
+            </tr>
+            <tr>
+              <td>오늘출발</td>
+              <td>0</td>
+              <td>건</td>
+              <td>배송완료</td>
+              <td>0</td>
+              <td>건</td>
+              <!-- <td>교환요청</td>
+              <td>0</td>
+              <td>건</td> -->
+            </tr>
+          </table>
+          @if(count($order))
+          <div class="sellerorderlist">
+            <!-- <form class="" action="index.html" method="post" name="mycheck"> -->
+            <div class="orderlist-bottom">
+              <button type="submit" name="button" id="check" class="ordercheck" form="order_list">발주확인</button>
+              <button type="submit" name="button" id="send" class="sendmessage" form="order_list">발송처리</button>
+            </div>
 
-    <!--button에 따라 action값 변경 -->
-    <!-- <form class="order_list" id="order_list" action="" method="post" onsubmit=""> -->
-    @csrf
+            <!--button에 따라 action값 변경 -->
+            <!-- <form class="order_list" id="order_list" action="" method="post" onsubmit=""> -->
+            @csrf
 
-    <table id="myTable">
-      <thead>
-        <tr>
-          <th class="title"> <input type="checkbox" name="checkAll" id="th_checkAll"  value=""> </th>
-          <th class="title">주문번호</th>
-          <th class="title">결제번호</th>
-          <th class="title">상품번호</th>
-          <th class="title">상품명</th>
-          <th class="title">수량</th>
-          <th class="title">송장번호</th>
-          <th class="title">택배사</th>
-          <!-- <th class="title">발송일</th> -->
-          <th class="title">주문일시</th>
-          <th class="title">고객명</th>
-          <th class="title">가격</th>
-          <th class="title">결제상태</th>
-          <th class="title">배송상태</th>
-          <!-- <th class="title"></th> -->
-        </tr>
-      </thead>
-      <tbody>
-        @foreach ($order as $order)
-        <tr>
-          <td><input type="checkbox" class="checkf" id="ordercheck{{$order->pm_no}}" name="checkRow" value=""></td>
-          <td>{{$order->o_no}}</td>
-          <td>{{$order->pm_no}}</td>
-          <td>{{$order->p_no}}</td>
-          <td id="p_name">{{$order->p_name}}</td>
-          <td>{{$order->pm_count}}</td>
-          <td><input type="text" class="num" id="invoice_num{{$order->pm_no}}" name="invoice_num"></td>
-          <td>
-            <select id="delivery" class="select" name=delivery margin-left:10px;>
-              <option value="">택배 선택</option>
-              <option value="우체국택배">우체국택배</option>
-              <option value="CJ대한통운">CJ대한통운</option>
-              <option value="로젠택배">로젠택배</option>
-              <option value="한진택배">한진택배</option>
-              <option value="현대택배">현대택배</option>
-              <option value="경동택배">경동택배</option>
-              <option value="KG로지스">KG로지스</option>
-              <option value="대신택배">대신택배</option>
-              <option value="합동택배">합동택배</option>
-              <option value="천일택배">천일택배</option>
-            </select></td>
-            <!-- <td>2020.04.16</td> -->
-            <td id="date">{{$order->created_at}}</td>
-            <td>{{$order->c_name}}</td>
-            <td>{{$order->pm_pay}}</td>
-            <td id="pm_status">{{$order->pm_status}}</td>
-            <td id="pm_d_status">{{$order->pm_d_status}}</td>
-            <!-- <td><button type="submit" name="button">저장</button></td> -->
-          </tr>
-          @endforeach
-        </tbody>
-      </table>
-      <!-- </form> -->
-    </div>
-    @else
-    <div class="flowercart-infor" id="remove" style="height:400px; position:relative;">
-      <div class="" style="top:180px; position:absolute; left:300px; ">
-        주문목록이 없습니다.
+            <table id="myTable">
+              <thead>
+                <tr>
+                  <th class="title"> <input type="checkbox" name="checkAll" id="th_checkAll"  value=""> </th>
+                  <th class="title">주문번호</th>
+                  <th class="title">결제번호</th>
+                  <th class="title">상품번호</th>
+                  <th class="title">상품명</th>
+                  <th class="title">수량</th>
+                  <th class="title">송장번호</th>
+                  <th class="title">택배사</th>
+                  <!-- <th class="title">발송일</th> -->
+                  <th class="title">주문일시</th>
+                  <th class="title">고객명</th>
+                  <th class="title">가격</th>
+                  <th class="title">결제상태</th>
+                  <th class="title">배송상태</th>
+                  <th class="title">배송조회</th>
+                  <!-- <th class="title"></th> -->
+                </tr>
+              </thead>
+              <tbody>
+                @foreach ($order as $order)
+                <tr>
+                  <td><input type="checkbox" class="checkf" id="ordercheck{{$order->pm_no}}" name="checkRow" value=""/></td>
+                  <td>{{$order->o_no}}</td>
+                  <td>{{$order->pm_no}}</td>
+                  <td>{{$order->p_no}}</td>
+                  <td id="p_name">{{$order->p_name}}</td>
+                  <td>{{$order->pm_count}}</td>
+
+                  <!--데이터값 존재x-->
+                  @if(!isset($order->pm_invoice_num))
+                  <td><input type="text" class="num" id="invoice_num{{$order->pm_no}}" name="invoice_num"></td>
+
+                  <!--데이터값 존재-->
+                  @else
+                  <td>
+                    <!-- <div id="re_inv_div" style="display:none;">
+                    <p id="re_invoice">{{$order->pm_invoice_num}}</p><input id="re_invoice_btn" class="re_btn" type="button" value="수정"/>
+                  </div> -->
+                  <div id="editform" name="editform">
+                    {{$order->pm_invoice_num}}
+                  </div>
+                  <div id="editbtn" name="editbtn">
+                    <button id="btn" name="btn" class="re_btn" type="button">수정</button>
+                  </div>
+                </td>
+                @endif
+
+                @if(!isset($order->pm_company))
+                <td id="select">
+                  <select id="delivery" class="select" name=delivery margin-left:10px;>
+                    <option value="">택배 선택</option>
+                    <option value="우체국택배" id="kr.epost" name="우체국 택배" tel="+8215881300">우체국택배</option>
+                    <option value="CJ대한통운" id="kr.cjlogistics" >CJ대한통운</option>
+                    <option value="로젠택배">로젠택배</option>
+                    <option value="한진택배">한진택배</option>
+                    <option value="현대택배">현대택배</option>
+                    <option value="경동택배">경동택배</option>
+                    <option value="KG로지스">KG로지스</option>
+                    <option value="대신택배">대신택배</option>
+                    <option value="합동택배">합동택배</option>
+                    <option value="천일택배">천일택배</option>
+                  </select></td>
+                  @else
+                  <td><p>{{$order->pm_company}}</p></td>
+                  @endif
+                  <!-- <td>2020.04.16</td> -->
+                  <td id="date">{{$order->created_at}}</td>
+                  <td>{{$order->c_name}}</td>
+                  <td>{{$order->pm_pay}}</td>
+                  <td id="pm_status">{{$order->pm_status}}</td>
+                  <td id="pm_d_status">{{$order->pm_d_status}}</td>
+
+                  @if(isset($order->pm_company))
+                  <td id="delivery_search"><a href="https://tracker.delivery/#/{{$order->delivery_code}}/{{$order->pm_invoice_num}}" target="_blank">배송조회</a></td>
+                  @else
+                  <td id="delivery_search"></td>
+                  @endif
+                  <!-- <td><button type="submit" name="button">저장</button></td> -->
+                </tr>
+                @endforeach
+              </tbody>
+            </table>
+            <!-- </form> -->
+          </div>
+          @else
+          <div class="flowercart-infor" id="remove" style="height:400px; position:relative;">
+            <div class="" style="top:180px; position:absolute; left:300px; ">
+              주문목록이 없습니다.
+            </div>
+          </div>
+          @endif
+        </div>
       </div>
     </div>
-    @endif
   </div>
-</div>
-</div>
-</div>
-@include('lib.footer')
+  @include('lib.footer')
 </body>
 </html>
 
@@ -177,8 +208,21 @@ $(document).ready(function(){
   $('#send').click(function () {
     // $('#order_list').attr("onsubmit", "return form_send()");
     // $('#order_list').attr("action", "/delivery_status");
+    //form_send();
     form_send();
   });
+
+  $('#btn').on('click',function () {
+    var text = $("#editform").text();
+    $('#editform').html("<input type='text' vlaue='"+text+"' id='editDo'>");
+    $('#editbtn').html("<button type='button' id='btnDo'>수정하기</button>")
+  });
+});
+
+$(document).on('click','#btnDo',function () {
+  update_invoice();
+  $('#editform').text($("#editDo").val());
+  $('#editbtn').html("<button type='button' id='btn'>수정</button>")
 });
 
 //전체 체크박스
@@ -208,7 +252,6 @@ for(var i=0; i<objs.length ; i++){
 function refreshMemList(){
   location.reload();
 }
-
 
 //발주확인
 function form_check(){
@@ -257,20 +300,61 @@ function form_check(){
         url: '/payment_status',
         dataType: 'json',
         data: { "check_on" : check_on },
-      success: function(data) {
-        console.log(data);
-        alert('변경되었습니다.');
-        refreshMemList();
-        // document.getElementById('pm_status').innerHTML="data";
-      },
-      error: function(data) {
-        console.log("error");
-      }
-    });
+        success: function(data) {
+          console.log(data);
+          alert('변경되었습니다.');
+          refreshMemList();
+          // document.getElementById('pm_status').innerHTML="data";
+        },
+        error: function(data) {
+          console.log("error");
+        }
+      });
+    }
   }
 }
-}
 
+function test(){
+  var invoiceJ = /^[0-9]*$/;
+  //indexe 배열
+  var checkbox_id = [];
+  //check가 된 ordercheck를 담을 배열
+  var check_on = [];
+  //pm_status 의 각 상태를 넣을 배열
+  var pm_status = [];
+  //송장번호를 넣을 배열
+  var invoice = [];
+  //선택된 택배지를 넣을 배열
+  var delivery = [];
+
+  $("input:checkbox[name=checkRow]:checked").each(function(index,elements)
+  {
+
+    //checkbox 각각의 id값
+    var index_no = elements.id;
+    checkbox_id.push(index_no);
+
+    pm_status.push($('#'+index_no).parent().parent().children('#d_status').text());
+
+    //송장번호 각각의 값
+    invoice.push($('#'+index_no).parent().parent().children().children('.num').val());
+
+    //선택된 택배지를 넣을 배열
+    delivery.push($('#'+index_no).parent().parent().children().children('.select').children('option:selected').val());
+    console.log($('#'+index_no).parent().parent().children().children('.select').children('option:selected').val());
+    console.log($('#'+index_no).parent().parent().children().children('.select').children('option:selected').parent("#id").id);
+
+
+    // console.log($('#'+index_no).parent().parent().children().children('.select').children('option:selected').parent('#id'));
+    // console.log($('#'+index_no).parent().parent().children().children('.select').children('option:selected').children('#id').val());
+    //console.log($('#'+index_no).parent().parent().children().children('.select').children('option:selected').val());
+
+    //숫자가 아닌부분 공백으로 치환 ---- payment_table no값
+    var pm_no = index_no.replace(/[^0-9]/g,"");
+    check_on.push(pm_no);
+    //console.log(pm_no);
+  });
+}
 
 //발송처리
 function form_send(){
@@ -300,14 +384,13 @@ function form_send(){
 
     //선택된 택배지를 넣을 배열
     delivery.push($('#'+index_no).parent().parent().children().children('.select').children('option:selected').val());
+    console.log($('#'+index_no).parent().parent().children().children('.select').children('option:selected').val('#id'));
     //console.log($('#'+index_no).parent().parent().children().children('.select').children('option:selected').val());
 
     //숫자가 아닌부분 공백으로 치환 ---- payment_table no값
     var pm_no = index_no.replace(/[^0-9]/g,"");
     check_on.push(pm_no);
     //console.log(pm_no);
-
-
   });
   //console.log(delivery);
   //송장번호 기입한거를 문자열로 합친것
@@ -364,7 +447,7 @@ function form_send(){
       success: function(data) {
         console.log(data);
         alert('변경되었습니다.');
-        refreshMemList();
+        //refreshMemList();
 
       },
       error: function(data) {
