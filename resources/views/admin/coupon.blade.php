cceb6f8e5bc7bbc80552e2f8d7f664b9b389a83d
sohyun <64099537+So-Hyun-Park@users.noreply.github.com>
2020-07-14 12:26:34 +0900

    .

--- resources/views/admin/coupon.blade.php
+++ resources/views/admin/coupon.blade.php
@@ -6,9 +6,6 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta name="description" content="">
-  <meta name="author" content="Dashboard">
-  <meta name="keyword" content="Dashboard, Bootstrap, Admin, Template, Theme, Responsive, Fluid, Retina">
   <title>꽃갈피 관리자</title>
   <link rel="stylesheet" href="//cdn.datatables.net/1.10.21/css/jquery.dataTables.min.css" type="text/css"/>
 
