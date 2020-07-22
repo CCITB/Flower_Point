d8872765f492b1e1b2dc62a0151ac710bd42c3de
jisuEO <58356312+GMSWnnk@users.noreply.github.com>
2020-07-23 02:32:42 +0900

    커맨드생성

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,7 +14,8 @@ class Kernel extends ConsoleKernel
   * @var array
   */
   protected $commands = [
-    Commands\DeliveryStatus::class
+    Commands\DeliveryStatus::class,
+    Commands\CouponStatus::class
   ];
 
   /**
@@ -27,6 +28,8 @@ class Kernel extends ConsoleKernel
   {
     $schedule->command('delivery:send')
     ->daily();
+    $schedule->command('coupon:send')
+    ->everyMinute();
   }
 
   /**
@@ -41,20 +44,3 @@ class Kernel extends ConsoleKernel
     require base_path('routes/console.php');
   }
 }
-
-// 배송중인 상품의 결제완료 시간
-// $complete_date = DB::table('payment')->pluck('pm_complete_date');
-// //현재시간
-// $today = Carbon::now();
-// $date_diff = [];
-// for($i=0; $i<count($complete_date); $i++){
-//   $date_diff[$i] = $today->diffInDays($complete_date[$i]);
-//
-//   //참고 : null일때도 차는 0임(당일과 같은 값)
-//   if($date_diff[$i]>=7){
-//     DB::table('payment')->where('pm_d_status','like','%배송중%')
-//     ->update([
-//       'pm_d_status'=>'배송 완료'
-//     ]);
-//   }//if
-// }//for
