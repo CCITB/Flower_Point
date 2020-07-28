22c741a95cf8ce853cfa19c00e420e939072e584
jisuEO <58356312+GMSWnnk@users.noreply.github.com>
2020-07-28 22:41:14 +0900

    .

--- app/Console/Commands/CouponStatus.php
+++ app/Console/Commands/CouponStatus.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use DB;
 
 class CouponStatus extends Command
 {
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -29,7 +29,7 @@ class Kernel extends ConsoleKernel
     $schedule->command('delivery:send')
     ->daily();
     $schedule->command('coupon:send')
-    ->hourlyAt(16);
+    ->hourlyAt(43);
   }
 
   /**
