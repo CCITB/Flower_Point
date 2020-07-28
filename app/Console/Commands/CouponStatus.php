6481cfeb6eca0b323f1e247d662ba77901dcd5c6
jisuEO <58356312+GMSWnnk@users.noreply.github.com>
2020-07-28 23:55:04 +0900

    TEST5

--- app/Console/Commands/CouponStatus.php
+++ app/Console/Commands/CouponStatus.php
@@ -41,7 +41,7 @@ class CouponStatus extends Command
       // DB::table('coupon')->where('end_date','<','NOW()')
       DB::table('coupon')
       ->select('*')
-      ->where('end_date','<','GETDATE()')
+      ->where('end_date','<',DB::raw('GETDATE()'))
       ->update([
       'cp_expiration'=>'Y'
       ]);
