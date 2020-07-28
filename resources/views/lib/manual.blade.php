f6f97f348d6d846283a7025bfb06ba8a82aaa9e4
kmkm37 <60097820+kmkm37@users.noreply.github.com>
2020-07-28 23:39:28 +0900

    .

--- resources/views/lib/manual.blade.php
+++ resources/views/lib/manual.blade.php
@@ -28,18 +28,18 @@
       </ul>
     </div>
     <div class="register_manual_h">
-      <img class="register_manual" id="register" src="/imglib/h.png" >
+      <img class="register_manual" id="register" src="/imglib/h.png" width="100%" height="100%">
     </div>
     <div class="invoice_manual_h">
-      <img class="invoice_manual" id="invoice_num" src="/imglib/a.png" >
-      <img class="invoice_manual" id="invoice_num1" src="/imglib/b.png" >
-      <img class="invoice_manual" id="invoice_num2" src="/imglib/c.png" >
+      <img class="invoice_manual" id="invoice_num" src="/imglib/a.png" width="100%" height="100%">
+      <img class="invoice_manual" id="invoice_num1" src="/imglib/b.png" width="100%" height="100%">
+      <img class="invoice_manual" id="invoice_num2" src="/imglib/c.png" width="100%" height="100%">
     </div>
     <div class="product_manual_h">
-      <img class="product_manual" id="product" src="/imglib/d.png" >
-      <img class="product_manual" id="product2" src="/imglib/e.png" >
-      <img class="product_manual" id="product3" src="/imglib/f.png" >
-      <img class="product_manual" id="product4" src="/imglib/g.png" >
+      <img class="product_manual" id="product" src="/imglib/d.png" width="100%" height="100%">
+      <img class="product_manual" id="product2" src="/imglib/e.png" width="100%" height="100%">
+      <img class="product_manual" id="product3" src="/imglib/f.png" width="100%" height="100%">
+      <img class="product_manual" id="product4" src="/imglib/g.png" width="100%" height="100%">
     </div>
   </div>
   @include('lib.footer')
